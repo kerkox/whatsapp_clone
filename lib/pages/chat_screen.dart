c8f71e5fdffe5fc91e0f260a38df4fd1ce54c210
Paul Cortes <paulkerg@gmail.com>
2019-04-01 08:44:25 -0500

    Ajustes para la vista del envio de mensajes

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -9,10 +9,38 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: Container(
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                  child: TextField(
+                controller: _textController,
+              )),
+              Container(
+                child: IconButton(icon: Icon(Icons.send), onPressed: () {}),
+              )
+            ],
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(widget.name),
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.name)),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+        ));
   }
 }
